0e94862868365bc71ea8f5287f8934c49edb1934
Charlie <30825964+SigSeg-V@users.noreply.github.com>
2023-07-29 19:38:17 -0700

    Created menu

--- Plugins/MultiplayerSessions/Source/MultiplayerSessions/Private/MultiplayerSessionsMenu.cpp
+++ Plugins/MultiplayerSessions/Source/MultiplayerSessions/Private/MultiplayerSessionsMenu.cpp
@@ -3,6 +3,9 @@
 
 #include "MultiplayerSessionsMenu.h"
 
+#include "MultiplayerSessionsSubsystem.h"
+#include "Components/Button.h"
+
 void UMultiplayerSessionsMenu::MenuSetup()
 {
 	AddToViewport();
@@ -24,4 +27,60 @@ void UMultiplayerSessionsMenu::MenuSetup()
 			Player->SetShowMouseCursor(true);
 		}
 	}
+	// access the subsystem by the game instance
+	const UGameInstance* GameInstance = GetGameInstance();
+	if (GameInstance != nullptr)
+	{
+		MultiplayerSessionsSubsystem = GameInstance->GetSubsystem<UMultiplayerSessionsSubsystem>();
+	}
+}
+
+void UMultiplayerSessionsMenu::HostButtonClicked()
+{
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(
+			-1,
+			15.f,
+			FColor::Green,
+			FString::Printf(TEXT("Clicked host button")));
+	}
+
+	if (MultiplayerSessionsSubsystem != nullptr)
+	{
+		MultiplayerSessionsSubsystem->CreateSession(6, "ffa");
+	}
+}
+
+void UMultiplayerSessionsMenu::JoinButtonClicked()
+{
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(
+			-1,
+			15.f,
+			FColor::Blue,
+			FString::Printf(TEXT("Clicked join button")));
+	}
+}
+
+bool UMultiplayerSessionsMenu::Initialize()
+{
+	if (!Super::Initialize())
+	{
+		return false;
+	}
+
+	// add callback methods to the buttons
+	if (HostButton != nullptr)
+	{
+		HostButton->OnClicked.AddDynamic(this, &ThisClass::HostButtonClicked);
+	}
+	
+	if (JoinButton != nullptr)
+	{
+		JoinButton->OnClicked.AddDynamic(this, &ThisClass::JoinButtonClicked);
+	}
+	
+	return true;
 }
--- Plugins/MultiplayerSessions/Source/MultiplayerSessions/Public/MultiplayerSessionsMenu.h
+++ Plugins/MultiplayerSessions/Source/MultiplayerSessions/Public/MultiplayerSessionsMenu.h
@@ -6,6 +6,8 @@
 #include "Blueprint/UserWidget.h"
 #include "MultiplayerSessionsMenu.generated.h"
 
+class UMultiplayerSessionsSubsystem;
+class UButton;
 /**
  * 
  */
@@ -15,6 +17,28 @@ class MULTIPLAYERSESSIONS_API UMultiplayerSessionsMenu : public UUserWidget
 	GENERATED_BODY()
 
 public:
+	
+	/**
+	 * @brief Sets up the usual defaults for a menu, free cursor, input removed from actor etc.
+	 */
 	UFUNCTION(BlueprintCallable)
 	void MenuSetup();
+
+private:
+
+	UPROPERTY(meta = (BindWidget))
+	UButton* HostButton;
+
+	UPROPERTY(meta = (BindWidget))
+	UButton* JoinButton;
+
+	UFUNCTION()
+	void HostButtonClicked();
+
+	UFUNCTION()
+	void JoinButtonClicked();
+
+	virtual bool Initialize() override;
+
+	UMultiplayerSessionsSubsystem* MultiplayerSessionsSubsystem;
 };
